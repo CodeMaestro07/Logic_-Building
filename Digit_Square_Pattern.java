963dedfc8b82e44d6ddd885a905a8bca81c64610
Yuvraj Gajanan Budhwat <yuvrajbudhwat@gmail.com>
2024-12-05 16:57:12 +0530

    Add files via upload

---
+++ Digit_Square_Pattern.java
@@ -0,0 +1,16 @@
+package com.logic.pattern;
+
+public class Digit_Square_Pattern {
+
+	public static void main(String[] args) {
+		int r = 3;
+		int c = 5;
+		for(int i = 1;i<=r;i++) {
+			for(int j=1;j<=c;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
