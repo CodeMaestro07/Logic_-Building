77d16494d299dcb943452a8c017b5ed69aa2633b
Yuvraj Gajanan Budhwat <yuvrajbudhwat@gmail.com>
2025-03-10 21:24:36 +0530

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,19 +1,22 @@
 package learningjavabasic;
 import java.util.*;
 public class Program {
-    public static void main(String[] args) {        
-        HashMap<String,String> countries = new HashMap<>();
-        countries.put("TH","Thailand");
-        countries.put("JP", "Japan");
-        countries.put("CN", "China");
-        countries.remove("CN");
-//        countries.clear();
-//        System.out.println(countries.containsValue("Lao"));
-        System.out.println(countries);
-        System.out.println(countries.size());
-        HashMap<Boolean,String> confirm = new HashMap<>();
-        confirm.put(true,"OK");
-        confirm.put(false, "Cancel");
-        System.out.println(confirm);
+    public static void main(String[] args) {
+//        HashSet<String> data = new HashSet<>();
+//        LinkedHashSet<String> data = new LinkedHashSet<>();
+        TreeSet<String> data = new TreeSet<>();
+        data.add("Java");
+        data.add("PHP");
+        data.add("Python");
+        data.add("C#");
+        data.add("Dart");
+        data.add("Assembly");
+        data.add("PHP");
+        data.add("R");
+        data.add("Go");
+//        data.remove("PHP");
+//        data.clear();
+        System.out.println(data);
+        System.out.println(data.size());
     }
 }
