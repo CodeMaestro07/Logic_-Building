6852f96185d141046191b74d593720e68cbc2cc9
Yuvraj Gajanan Budhwat <yuvrajbudhwat@gmail.com>
2025-03-11 21:24:29 +0530

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,22 +1,16 @@
 package learningjavabasic;
-import java.util.*;
+class Item<T extends Number>{
+    T data;
+    public Item(T value){
+        data=value;
+    }
+}
 public class Program {
     public static void main(String[] args) {
-//        HashSet<String> data = new HashSet<>();
-//        LinkedHashSet<String> data = new LinkedHashSet<>();
-        TreeSet<String> data = new TreeSet<>();
-        data.add("Java");
-        data.add("PHP");
-        data.add("Python");
-        data.add("C#");
-        data.add("Dart");
-        data.add("Assembly");
-        data.add("PHP");
-        data.add("R");
-        data.add("Go");
-//        data.remove("PHP");
-//        data.clear();
-        System.out.println(data);
-        System.out.println(data.size());
+        Item<Integer> obj1=new Item<>(99);
+        System.out.println(obj1.data);
+        Item<Double> obj2=new Item<>(10.5);
+        System.out.println(obj2.data);
     }
+
 }
