7b789796454ce4b12e3214a4bcc8190281da4e33
Yuvraj Gajanan Budhwat <yuvrajbudhwat@gmail.com>
2025-03-11 21:25:03 +0530

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,5 +1,5 @@
 package learningjavabasic;
-class Item<T extends Number>{
+class Item<T>{
     T data;
     public Item(T value){
         data=value;
@@ -11,6 +11,12 @@ public class Program {
         System.out.println(obj1.data);
         Item<Double> obj2=new Item<>(10.5);
         System.out.println(obj2.data);
+        Item<String> obj3 = new Item<>("Kong");
+        System.out.println(obj3.data);
+        Item<Boolean> obj4=new Item<>(true);
+        System.out.println(obj4.data);
+        Item<Character> obj5=new Item<>('C');
+        System.out.println(obj5.data);
     }
 
 }
